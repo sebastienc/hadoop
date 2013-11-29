85c4a8d065e0fc50e87db32caf72b1e84f25b028
Philippe Laflamme <plaflamme@hopper.com>
2013-11-29 09:19:58 -0500

    Wrap SocketAppender in an AsyncAppender. Also log to TaskLogAppender to get default behaviour.

--- src/main/java/org/apache/hadoop/mapred/TaskSocketAppender.java
+++ src/main/java/org/apache/hadoop/mapred/TaskSocketAppender.java
@@ -1,48 +1,73 @@
 package org.apache.hadoop.mapred;
 
-import java.net.InetAddress;
-
+import org.apache.log4j.AsyncAppender;
 import org.apache.log4j.net.SocketAppender;
 import org.apache.log4j.spi.LoggingEvent;
 
-public class TaskSocketAppender extends SocketAppender {
+public class TaskSocketAppender extends AsyncAppender {
 
 	static final String TASKID_PROPERTY = "hadoop.tasklog.taskid";
 
 	private final TaskAttemptID taskId;
 
-	public TaskSocketAppender() {
-		taskId = extractTaskId();
-	}
-	
-	public TaskSocketAppender(InetAddress address, int port) {
-		  super(address, port);
-			taskId = extractTaskId();
-	}
-	
-	public TaskSocketAppender(String host, int port) {
-		super(host, port);
-		taskId = extractTaskId();
-	}
+	/** SocketAppender properties */
+	private int port;
+
+	private String remoteHost;
 
 	static final TaskAttemptID extractTaskId() {
 		String str = System.getProperty(TASKID_PROPERTY);
-		if(str != null) {
-		    return TaskAttemptID.forName(str);
+		if (str != null) {
+			return TaskAttemptID.forName(str);
 		} else {
 			return null;
 		}
 	}
 
+	public TaskSocketAppender() {
+		taskId = extractTaskId();
+		super.setBlocking(false);
+	}
+
+	public String getRemoteHost() {
+		return remoteHost;
+	}
+
+	public void setRemoteHost(String remoteHost) {
+		this.remoteHost = remoteHost;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public void activateOptions() {
+		addAppender(new SocketAppender(remoteHost, port));
+		addAppender(newTaskLogAppender());
+		super.activateOptions();
+	}
+
 	@Override
 	public void append(LoggingEvent event) {
-		if(event == null)
-		      return;
-		if(taskId != null) {
-		  event.setProperty("task_attempt_id", taskId.toString());
-		  event.setProperty("task_id", taskId.getTaskID().toString());
-		  event.setProperty("job_id", taskId.getJobID().toString());
+		if (event == null)
+			return;
+		if (taskId != null) {
+			event.setProperty("task_attempt_id", taskId.toString());
+			event.setProperty("task_id", taskId.getTaskID().toString());
+			event.setProperty("job_id", taskId.getJobID().toString());
 		}
 		super.append(event);
+	}
+
+	private TaskLogAppender newTaskLogAppender() {
+		TaskLogAppender tla = new TaskLogAppender();
+		tla.setLayout(getLayout());
+		tla.activateOptions();
+		return tla;
 	}
 }
